825ac20b5315561dd64ebb9a8700ea12e86608b4
Caio Augusto <55853613+LopezTw@users.noreply.github.com>
2023-03-08 21:30:07 -0300

    Update App.java

--- Exercicio7/src/App.java
+++ Exercicio7/src/App.java
@@ -1,6 +1,8 @@
 // Leia uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. 
 // A fórmula de conversão é: F=(9*C+160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius;
 
+// Eu não precisava usar WHILE, mas usei a fim de tornar mais útil
+
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
