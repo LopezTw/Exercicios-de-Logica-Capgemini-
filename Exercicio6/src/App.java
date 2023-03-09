68be243c679280786ce7a8512e80c55b33f291f1
LopezTw <caiohg_94@hotmail.com>
2023-03-08 21:25:50 -0300

    Exercicio Resolvido

--- Exercicio6/src/App.java
+++ Exercicio6/src/App.java
@@ -1,3 +1,6 @@
+//Leia dois valores para as variáveis A e B, e efetuar as trocas dos valores de forma que a variávelApasse a 
+//possuir ovalor da variável B e a variável B passe a possuir o valor da variável A.Apresentar osvalores 
+//trocados;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
@@ -6,12 +9,12 @@ public class App {
         int b = 0;
         int temp = 0;
 
-        Scanner leitura = new Scanner(System.in);
-
-        System.out.println("Digita o valor de A: ");
-        a = leitura.nextInt();
-        System.out.println("Digita o valor de B: ");
-        b = leitura.nextInt();
+        try (Scanner leitura = new Scanner(System.in)) {
+            System.out.println("Digita o valor de A: ");
+            a = leitura.nextInt();
+            System.out.println("Digita o valor de B: ");
+            b = leitura.nextInt();
+        }
 
         temp = a;
         a = b;
