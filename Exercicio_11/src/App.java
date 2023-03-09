32ead370671b647dcd696fe87d169a510f76d786
LopezTw <caiohg_94@hotmail.com>
2023-03-09 10:44:00 -0300

    Exercicio Resolvido

--- Exercicio_11/src/App.java
+++ Exercicio_11/src/App.java
@@ -1,5 +1,27 @@
+// Faça um algoritmo que receba o preço de custo de um produto e mostre o valor de venda. Sabe-se que o 
+// preço de custo receberá um acréscimo de acordo com um percentual informado pelo usuário;
+
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        float precoCusto = 0.0f;
+        float percentual = 0.0f;
+        float precoVenda = 0.0f;
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Informe o preço de Custo do produto: ");
+        precoCusto = leitor.nextFloat();
+        System.out.print("Digite o percentual de acréscimo para o preço de venda (em %): ");
+        percentual = leitor.nextFloat();
+
+        // Essa parte da expressão adiciona o valor decimal do percentual ao valor 1.
+        precoVenda = precoCusto * (1 + (percentual / 100)); 
+
+        System.out.println("O preço de venda do produto é: R$" + precoVenda);
+
+        //leitor.close();
+
     }
 }
