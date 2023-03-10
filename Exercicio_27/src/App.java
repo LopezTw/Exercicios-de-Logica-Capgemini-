2c73696f6124423ac12530b46256b0b0fa86e95c
Caio Augusto <55853613+LopezTw@users.noreply.github.com>
2023-03-10 16:48:28 -0300

    Estrutura SWITCH

--- Exercicio_27/src/App.java
+++ Exercicio_27/src/App.java
@@ -2,7 +2,7 @@
 //algoritmo que calcule e exiba o valor do desconto e o valor a ser pago pelo cliente. O desconto deverá ser 
 //calculado sobre o valor do veículo de acordo com o combustível (álcool – 25%, gasolina – 21% ou diesel 
 //–14%). Com valor do veículo zero encerra entrada de dados. Informe total de desconto e total pago pelos 
-//clientes;
+//clientes;;
 
 import java.util.Scanner;
 public class App {
